ed0ca574afdbe2ad5d1f4a481563f6f96b59b13f
Felicia <190894036+feliJS@users.noreply.github.com>
2025-05-12 14:55:33 +0200

    First server test
    Fetches the request to the endpoint /cities and outputs if it works or nay, if it works it will return the json response and then output it

---
+++ test.js
@@ -0,0 +1,14 @@
+//GLÖM EJ FIXA INDENTSEN!
+
+//#1
+let getCities = new Request("https://localhost:8000/cities")
+fetch(getCities)
+    .then((response) => {
+        if(response.status == ok){
+            console.log("everything works, " + response.status);
+            return response.json()
+        }
+    })
+    .then((allCities) => {
+        console.log(allCities);
+    })
