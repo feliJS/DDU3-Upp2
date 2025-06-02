cfe1ad09430210481421f48da8249fd1192774a4
Felicia <190894036+feliJS@users.noreply.github.com>
2025-06-02 11:28:07 +0200

    Funktioner istället

--- static/test.js
+++ static/test.js
@@ -1,166 +1,172 @@
-//GLÖM EJ FIXA INDENTSEN!
-//ta bort everything works loggen?
+// #1
+function fetchCities() {
+    let getCities = new Request("http://localhost:1337/cities");
+    return fetch(getCities)
+        .then(response => {
+            if (response.status == 200) {
+                console.log("everything works, " + response.status);
+                return response.json();
+            }
+        })
+        .then(allCities => {
+            console.log(allCities);
+        });
+}
 
-//#1
-let getCities = new Request("http://localhost:1337/cities")
-fetch(getCities)
-    .then((response) => {
-        if(response.status == 200){
-            console.log("everything works, " + response.status);
-            return response.json()
-        }
-    })
-    .then((allCities) => {
-        console.log(allCities);
-    })
+// #2
+function postCity() {
+    let citiesPost = new Request("http://localhost:1337/cities", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: 43,
+            name: "Malmö",
+            country: "Sweden"
+        })
+    });
+    return fetch(citiesPost)
+        .then(response => {
+            if (response.status == 200) {
+                console.log("everything works, " + response.status);
+                return response.json();
+            }
+        })
+        .then(addedCity => {
+            console.log(addedCity);
+        });
+}
 
-//#2
-let citiesPost = new Request("http://localhost:1337/cities", {method: "POST", headers:{
-    "Content-Type": "application/json"},    
-    body: JSON.stringify({
-        "name": "Malmö",
-        "country": "Sweden"
-    })})
-fetch(citiesPost)
-    .then((response) => {
-        if(response.status == 200){
-            console.log("everything works, " + response.status);
-            return response.json()
-        }
-    })
-    .then((addedCity) => {
-        console.log(addedCity);
-    })
+// #3
+function deleteCityById() {
+    let deleteCity = new Request("http://localhost:1337/cities", {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ id: 2 })
+    });
+    return fetch(deleteCity)
+        .then(response => {
+            if (response.status == 200) {
+                console.log("everything works, " + response.status);
+                return response.text();
+            }
+        })
+        .then(message => {
+            console.log(message);
+        });
+}
 
-//#3
-let deleteCity = new Request("http://localhost:1337/cities", {
-    method: "DELETE",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-        id: 2
-    })
-})
-fetch(deleteCity)
-    .then((response) => {
-        if(response.status == 200){
-            console.log("everything works, " + response.status);
-            return response.text()
-        }
-    })
-    .then((message) => {
-        console.log(message);
-    })
-
-//#4
+// #4
 async function getCitiesAgain() {
-    let response = await fetch("http://localhost:1337/cities")
-    if(response.status == 200){
+    let response = await fetch("http://localhost:1337/cities");
+    if (response.status == 200) {
         console.log("everything works, " + response.status);
-        let allCities = await response.json()
+        let allCities = await response.json();
         console.log(allCities);
     }
 }
-getCitiesAgain()
 
-//#5
+// #5
 async function getMalmo() {
-    let response = await fetch("http://localhost:1337/cities/43")
-    if(response.status == 200){
+    let response = await fetch("http://localhost:1337/cities/43");
+    if (response.status == 200) {
         console.log("everything works, " + response.status);
-        let city = await response.json()
+        let city = await response.json();
         console.log(city);
     }
 }
-getMalmo()
 
-
-//#6
+// #6
 async function searchCitiesEn() {
-    let response = await fetch("http://localhost:1337/cities/search?text=en")
-    if(response.status == 200){
+    let response = await fetch("http://localhost:1337/cities/search?text=en");
+    if (response.status == 200) {
         console.log("everything works, " + response.status);
-        let result = await response.json()
+        let result = await response.json();
         console.log(result);
     }
 }
-searchCitiesEn()
 
-//#7
+// #7
 async function searchCitiesEnSweden() {
-    let response = await fetch("http://localhost:1337/cities/search?text=en&country=Sweden")
-    if(response.status == 200){
+    let response = await fetch("http://localhost:1337/cities/search?text=en&country=Sweden");
+    if (response.status == 200) {
         console.log("everything works, " + response.status);
-        let result = await response.json()
+        let result = await response.json();
         console.log(result);
     }
 }
-searchCitiesEnSweden()
 
+// #8
+function duplicateCityPost() {
+    let duplicatePost = new Request("http://localhost:1337/cities", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: "Dresden",
+            country: "Germany"
+        })
+    });
+    return fetch(duplicatePost)
+        .then(response => {
+            console.log("everything works, " + response.status);
+        });
+}
 
-//#8
-let duplicatePost = new Request("http://localhost:1337/cities", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-        name: "Dresden",
-        country: "Germany"
-    })
-})
-fetch(duplicatePost)
-    .then((response) => {
-        console.log("status code is: " + response.status);
-    })
+// #9
+function invalidCityPost() {
+    let invalidPost = new Request("http://localhost:1337/cities", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: "Ystad"
+        })
+    });
+    return fetch(invalidPost)
+        .then(response => {
+            console.log("everything works, " + response.status);
+        });
+}
 
-//#9
-let invalidPost = new Request("http://localhost:1337/cities", {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-        name: "Ystad"
-    })
-})
-fetch(invalidPost)
-    .then((response) => {
-        console.log("status code is: " + response.status);
-    })
+// #10
+function deleteNonExistingCity() {
+    let deleteNonExistent = new Request("http://localhost:1337/cities", {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            id: 56
+        })
+    });
+    return fetch(deleteNonExistent)
+        .then(response => {
+            console.log("everything works, " + response.status);
+        });
+}
 
+// #11
+function deleteEmptyRequest() {
+    let deleteEmpty = new Request("http://localhost:1337/cities", {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({})
+    });
+    return fetch(deleteEmpty)
+        .then(response => {
+            console.log("everything works, " + response.status);
+        });
+}
 
-//#10
-let deleteNonExistent = new Request("http://localhost:1337/cities", {
-    method: "DELETE",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-        id: 56
-    })
-})
-fetch(deleteNonExistent)
-    .then((response) => {
-        console.log("status code is: " + response.status);
-    })
-
-
-//#11
-let deleteEmpty = new Request("http://localhost:1337/cities", {
-    method: "DELETE",
-    headers: {
-        "Content-Type": "application/json"
-    },
-    body: JSON.stringify({})
-})
-fetch(deleteEmpty)
-    .then((response) => {
-        console.log("status code is: " + response.status);
-    })
-
-//#12
+// #12
 async function postInvalidMessage() {
     let response = await fetch("http://localhost:1337/messages", {
         method: "POST",
@@ -172,23 +178,40 @@ async function postInvalidMessage() {
             to: 1,
             password: "pass"
         })
-    })
-    console.log("status code is: " + response.status);
+    });
+    console.log("everything works, " + response.status);
 }
-postInvalidMessage()
 
-//#13
+// #13
 async function getSearchWithoutQuery() {
-    let response = await fetch("http://localhost:1337/cities/search")
-    console.log("status code is: " + response.status);
+    let response = await fetch("http://localhost:1337/cities/search");
+    console.log("everything works, " + response.status);
 }
-getSearchWithoutQuery()
 
-//#14
+// #14
 async function deleteMordor() {
     let response = await fetch("http://localhost:1337/mordor", {
         method: "DELETE"
-    })
-    console.log("status code is: " + response.status);
+    });
+    console.log("everything works, " + response.status);
 }
-deleteMordor()
+
+// Kör alla i ordning
+async function runAll() {
+    await fetchCities();
+    await postCity();
+    await deleteCityById();
+    await getCitiesAgain();
+    await getMalmo();
+    await searchCitiesEn();
+    await searchCitiesEnSweden();
+    await duplicateCityPost();
+    await invalidCityPost();
+    await deleteNonExistingCity();
+    await deleteEmptyRequest();
+    await postInvalidMessage();
+    await getSearchWithoutQuery();
+    await deleteMordor();
+}
+
+runAll();
